ef5ed205affaecd610d7f9c473fc8e6b3a9179c3
1n3Tech <waveofsilenc2@gmail.com>
2023-06-25 15:57:50 +0100

    Changes

---
+++ src/components/LandingNav.tsx
@@ -0,0 +1,12 @@
+const LandingNav = () => {
+
+
+    return (
+      <div className="navbar bg-base-100 justify-between w-full">
+    <a className="btn btn-ghost normal-case text-xl">Bounce Hub</a>
+    <button className="btn btn-square btn-ghost">Login</button>
+</div>
+      )
+}
+
+export default LandingNav
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import LandingNav from "../components/LandingNav"
+const Home = () => {
+  
+  return (
+    <div className="max-w-4xl mx-auto">
+      <LandingNav />
+    </div>
+    
+  )
+}
+
+export default Home
